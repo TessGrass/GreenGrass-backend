aa56561f83ed2db0fb0281f4eb850a63029764c2
Therese Grass <tg222kv@student.lnu.se>
2022-04-21 08:07:01 +0200

    ready to deploy

--- src/routes/api/v1/image-router.js
+++ src/routes/api/v1/image-router.js
@@ -9,10 +9,10 @@ const chartController = new ChartController()
 const authController = new AuthController()
 router.get('/images', (req, res, next) => controller.getInfo(req, res, next))
 router.get('/chart/:id', (req, res, next) => chartController.getChartInfo(req, res, next))
-// router.post('/chart', (req, res, next) => chartController.postChartInfo(req, res, next))
 router.post('/chart', authController.authenticateToken, chartController.postChartData)
 router.put('/chart', authController.authenticateToken, chartController.putChartData)
-// router.post('/images', (req, res, next) => controller.postInfo(req, res, next))
 
+// router.post('/chart', (req, res, next) => chartController.postChartInfo(req, res, next))
+// router.post('/images', (req, res, next) => controller.postInfo(req, res, next))
 /* router.delete('/images/:id', (req, res, next) => controller.deleteSpecificImage) */
 /* router.get('/images/:id', (req, res, next) => controller.getSpecificImage) */
