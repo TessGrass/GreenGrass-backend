f109da3f78bf01a3072de6b0b0dd9540a9cca1ce
Therese Grass <tg222kv@student.lnu.se>
2022-04-21 08:32:21 +0200

    added route

--- src/routes/router.js
+++ src/routes/router.js
@@ -4,6 +4,7 @@ import { router as v1Router } from './api/v1/router.js'
 
 export const router = express.Router()
 
+router.get('/', (req, res) => res.json({ message: 'This bad boy runs!' }))
 router.use('/api/v1', v1Router)
 
 // Catch 404 (ALWAYS keep this as the last route).
