52183eea2b8b4527aaa495aa2647586a40edb613
Therese Grass <tg222kv@student.lnu.se>
2022-05-17 09:01:05 +0200

    implemented authenticateToken to all endpoints

--- src/routes/api/v1/image-router.js
+++ src/routes/api/v1/image-router.js
@@ -7,23 +7,10 @@ export const router = express.Router()
 const chartController = new ChartController()
 const authController = new AuthController()
 const todoController = new TodoController()
-/* router.get('/images', (req, res, next) => controller.getInfo(req, res, next)) */
-/* router.get('/chart/:id', (req, res, next) => chartController.getChartData(req, res, next)) */
 
 router.get('/chart/:id', authController.authenticateToken, chartController.getChartData)
-router.post('/chart', authController.authenticateToken, chartController.updateChartData)
 router.get('/todo/:id', todoController.getTodoData)
-router.post('/todo', todoController.postTodoData)
-router.patch('/todo/:id', todoController.patchTodoData)
-router.delete('/todo/:id', todoController.deleteTodoData)
-/* router.put('/chart', authController.authenticateToken, chartController.putChartData) */
-
-// router.post('/chart', (req, res, next) => chartController.postChartInfo(req, res, next))
-// router.post('/images', (req, res, next) => controller.postInfo(req, res, next))
-/* router.delete('/images/:id', (req, res, next) => controller.deleteSpecificImage) */
-/* router.get('/images/:id', (req, res, next) => controller.getSpecificImage) */
-
-/* 1. Skrota image-controllern helt?
-2. try / catch måste implementeras
-3. testa/skicka statuskoder
-4. Sätt kontroll på get */
+router.post('/chart', authController.authenticateToken, chartController.updateChartData)
+router.post('/todo', authController.authenticateToken, todoController.postTodoData)
+router.patch('/todo/:id', authController.authenticateToken, todoController.patchTodoData)
+router.delete('/todo/:id', authController.authenticateToken, todoController.deleteTodoData)
