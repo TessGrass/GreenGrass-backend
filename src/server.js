96ff9c772a031edae3f3c3039a98ff51a77382d2
Therese Grass <tg222kv@student.lnu.se>
2022-05-20 11:22:18 +0200

    change error handeling in server.js

--- src/server.js
+++ src/server.js
@@ -21,18 +21,25 @@ try {
   app.use('/', router)
   app.use(function (err, req, res, next) {
     if (err.status === 400) {
-      res.status(400).json({ status_code: 400, message: 'The request cannot or will not be processed due to something that is perceived to be a client error (for example, validation error).' })
+      err.message = 'The request cannot or will not be processed due to something that is perceived to be a client error (for example, validation error).'
     } else if (err.status === 401) {
-      res.status(401).json({ status_code: 401, message: 'Access token invalid or not provided.' })
+      err.message = 'Access token invalid or not provided.'
     } else if (err.status === 403) {
-      res.status(403).json({ status_code: 403, message: 'The request contained valid data and was understood by the server, but the server is refusing action due to the authenticated user not having the necessary permissions for the resource.' })
+      err.message = 'The request contained valid data and was understood by the server, but the server is refusing action due to the authenticated user not having the necessary permissions for the resource.'
     } else if (err.status === 404) {
-      res.status(404).json({ status_code: 404, message: 'The requested resource was not found.' })
+      err.message = 'The requested resource was not found.'
     } else if (err.status || err.status === 500) {
-      res.status(500).json({ status_code: 500, message: 'An unexpected condition was encountered.' })
+      err.message = 'An unexpected condition was encountered.'
     }
 
     if (req.app.get('env') !== 'development') {
+      return res
+        .status(err.status)
+        .json({
+          status: err.status,
+          message: err.message
+        })
+    } else {
       return res
         .status(err.status)
         .json({
